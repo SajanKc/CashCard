161018d0918d7cd028ce375a223d84544fbeb438
sajan96kc <sajan1996kc@gmail.com>
2024-08-30 11:47:26 +0545

    upgrade: spring security config

--- src/main/java/com/iamsajan/cashcardservice/SecurityConfig.java
+++ src/main/java/com/iamsajan/cashcardservice/SecurityConfig.java
@@ -3,29 +3,33 @@ package com.iamsajan.cashcardservice;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests((authz) ->
-                        authz
-                                .antMatchers("/cashcards/**").hasRole("CARD-OWNER")
-                                .antMatchers("/h2-console/**").permitAll()
-                )
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .antMatchers("/cashcards/**").hasRole("CARD-OWNER")
+//                .antMatchers("/h2-console/**").permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/cashcards/**")).hasRole("CARD-OWNER")
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                .and()
                 .csrf().disable()
                 .httpBasic(withDefaults());
+        return http.build();
     }
 
     @Bean
---
+++ src/main/java/com/iamsajan/cashcardservice/upgrade-notes.md
@@ -0,0 +1,24 @@
+# 3.1 Upgrade Notes
+
+## Initial baseline
+
+- Code compiled successfully
+    - errors
+    - Deprecation warnings
+        -  src/main/java/example/cashcard/SecurityConfig.java uses or overrides a deprecated API.
+- found disabled/broken test - CashCardApplicationTests#shouldReturnACashCardWhenDataIsSaved
+    - fixed broken test and re-enabled
+
+_Result:_ code compiles without errors or warnings and all tests pass
+
+## Major Release Considerations
+- Upgrade Spring Security to `5.8.5`
+- Spring Security deprecations
+    - [INFO] /course-spring-boot-2-7-to-3-1-upgrade-code/src/main/java/com/iamsajan/cashcardservice/SecurityConfig.java: /course-spring-boot-2-7-to-3-1-upgrade-code/src/main/java/com/iamsajan/cashcardservice/SecurityConfig.java uses or overrides a deprecated API.
+    - [INFO] /course-spring-boot-2-7-to-3-1-upgrade-code/src/main/java/com/iamsajan/cashcardservice/SecurityConfig.java
+    - Reference: https://docs.spring.io/spring-security/reference/5.8/migration/index.html
+
+- Replace `WebSecurityConfigurerAdapter` super class with `SecurityFilterChain` bean definition
+- Replace `antMatcher` with `requestMatcher` builder method
+- Add `@Configuration` annotation, which is removed in Spring Security 6.0
+_Result:_ code compiles without errors or warnings and all tests pass (no skipped tests)!

376686b8f25d1eb9dfb1241c9fb7a8bb8f8a7827
sajan96kc <sajan1996kc@gmail.com>
2024-08-30 08:29:20 +0545

    change spring security config for upgrade

--- src/main/java/com/iamsajan/cashcardservice/SecurityConfig.java
+++ src/main/java/com/iamsajan/cashcardservice/SecurityConfig.java
@@ -2,28 +2,30 @@ package com.iamsajan.cashcardservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("/cashcards/**")
-                        .hasRole("CARD-OWNER")) // enable RBAC: Replace the .authenticated() call with the hasRole(...) call.
-                .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable());
-        return http.build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests((authz) ->
+                        authz
+                                .antMatchers("/cashcards/**").hasRole("CARD-OWNER")
+                                .antMatchers("/h2-console/**").permitAll()
+                )
+                .csrf().disable()
+                .httpBasic(withDefaults());
     }
 
     @Bean
